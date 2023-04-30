3dbef01aad536d43b8107e5963a977caf4bee499
Robert  Stevens <Rstevens318@gmail.com>
2023-04-30 10:22:24 -0400

    Final code. review once more if necessary

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -22,128 +22,155 @@ Base.prepare(autoload_with=engine)
 measurement = Base.classes.measurement
 station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-
-
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-
-
 #################################################
 # Flask Routes
 #################################################
-@app.route("/")
+
+#Define Homepage Route with a list of all available routes
+#Use a function to return a homepage .html template file
+@app.route("/", methods=['GET', 'POST'])
 def welcome():
     """List all available API routes."""
-    return (
-    #return render_template('home.html')
-    f"Welcome to the Hawaii Climate Analysis API!<br/>"
-    f"Available Routes:<br/>"
-    f"/api/v1.0/precipitation<br/>"
-    f"/api/v1.0/stations<br/>"
-    f"/api/v1.0/tobs<br/>"
-    f"/api/v1.0/<start>enter start date<br/>"
-    f"/api/v1.0/<start>/<end> enter start and end date")
-
+    return render_template('home.html')
 
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
+
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #Find the most recent date in our DB and convert to string format
     recent_date = session.query(func.max(measurement.date)).scalar()
     recent_date = dt.datetime.strptime(recent_date, "%Y-%m-%d").date()
+
+    #Calculate the date 1 year ago from the most recent date
     year_ago = recent_date - dt.timedelta(days=365)
+
+    #Query for the date and precipitation for the last year of data
     results = session.query(measurement.date, measurement.prcp).filter(measurement.date >= year_ago).all()
     
+    #Close the session
     session.close()
 
+    #Create a dictionary from the provided data and append to a list of the precipitation data
     all_prcp = []
+
     for date, prcp in results:
         prcp_dict = {}
         prcp_dict[date] = prcp
         all_prcp.append(prcp_dict)
 
+    #Return the JSON representation of your dictionary.
     return jsonify(all_prcp)
 
 @app.route('/api/v1.0/stations')
 def stations():
+
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #Query all stations
     results = session.query(station.station).all()
 
+    #Close the session
     session.close()
 
+    #Convert to flattened array
     all_stations = list(np.ravel(results))
 
+    #Return a JSON list of stations from the dataset.
     return jsonify(all_stations)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
+
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #Find the most recent date in our DB and convert to string format
     recent_date = session.query(func.max(measurement.date)).scalar()
     recent_date = dt.datetime.strptime(recent_date, "%Y-%m-%d").date()
+
+    #Calculate the date 1 year ago from the most recent date
     year_ago = recent_date - dt.timedelta(days=365)
 
+    #Find the most active station
     active_station = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
 
+    #Query the dates and temperature observations of the most active station for the last year of data.
     results = session.query(measurement.date, measurement.tobs).filter(measurement.station == active_station[0][0]).filter(measurement.date >= year_ago).all()
 
+    #Close the session
     session.close()
 
+    #Create a dictionary from the provided data and append to a list of the temperature data
     all_tobs = []
+
     for date, tobs in results:
         tobs_dict = {}
         tobs_dict[date] = tobs
         all_tobs.append(tobs_dict)
 
+    #Return the JSON representation of your dictionary.
     return jsonify(all_tobs)
 
 @app.route('/api/v1.0/<start>')
 def start(start):
+ 
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= start).all()
+    #Query for the dates and temperature observations from the start date.
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= start).group_by(measurement.date).all()
 
+    #Close the session
     session.close()
 
+    #Create a dictionary from the provided start date data and append to a list of the start date data
     all_start = []
-    data = True
+    
     for min, max, avg in results:
-        start_dict = {}
-        start_dict["min"] = min
-        start_dict["max"] = max
-        start_dict["avg"] = avg
-        all_start.append(start_dict)
+            start_dict = {}
+            start_dict["min"] = min
+            start_dict["max"] = max
+            start_dict["avg"] = round(avg,2)
+            all_start.append(start_dict)
 
-    if data == False:
-        return f"Data not found for the given date range. Please enter a new date range."
-    else:
-        return jsonify(all_start)
+    #Return the JSON representation of your dictionary.
+    return jsonify(all_start)
 
 @app.route('/api/v1.0/<start>/<end>')
 def start_end(start, end):
+
+    # Create our session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).all()
-    session.close
 
+    #Query for the dates and temperature observations in the given range.
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.date >= start,measurement.date <= end).group_by(measurement.date).all()
+    
+    #Close the session
+    session.close()
+
+    #Create a dictionary from the provided range data and append to a list of the range data
     from_start_end = []
-    data = True
+    
     for min, max, avg in results:
-        start_end_dict = {}
-        start_end_dict["min"] = min
-        start_end_dict["max"] = max
-        start_end_dict["avg"] = avg
-        from_start_end.append(start_end_dict)
+            start_end_dict = {}
+            start_end_dict["min"] = min
+            start_end_dict["max"] = max
+            start_end_dict["avg"] = round(avg,2)
+            from_start_end.append(start_end_dict)
+    
+    #Return the JSON representation of your dictionary.
+    return jsonify(from_start_end)
 
-    if data == False:
-        return f"Data not found for the given date range. Please enter a new date range."
-    else:
-        return jsonify(from_start_end)
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
